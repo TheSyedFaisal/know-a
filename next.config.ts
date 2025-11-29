0ba1d7b7085ca2dd3ca0dd67ae2682806413134d
TheSyedFaisal <fazalfaisal911@gmail.com>
2025-11-29 19:11:07 +0500

    Update Next.js config for proper video file handling

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,23 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  staticPageGenerationTimeout: 180,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/media/',
+            outputPath: `${__dirname}/public/`,
+            name: '[name].[hash].[ext]',
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
